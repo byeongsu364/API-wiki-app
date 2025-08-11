46976d0c169edb5d3300bb9bffc00359e6f8d5b1
byeongsu364 <kbsyjysbs@gmail.com>
2025-08-11 11:15:39 +0900

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,67 @@ import { useState, useRef, useEffect } from 'react'
 import { searchWikiTitles } from './api/wiki'
 
 function App() {
-
   const [q, setQ] = useState('')
-  const [items, serItems] = useState([])
+  const [items, setItems] = useState([])
   const [loading, setLoading] = useState(false)
   const [err, setErr] = useState('')
   const inputRef = useRef(null)
 
-  useEffect(()=>{
+  useEffect(() => {
     inputRef.current.focus()
-  },[])
+    console.log(items)
 
-  console.log(q)
+  }, [])
+
+  const handleSearch = async () => {
+    const keyword = q.trim()
+
+    if (!keyword) return
+
+    try {
+      setLoading(true)
+      setErr('')
+
+      const pages = await searchWikiTitles(keyword)
+      setItems(pages)
+    } catch (error) {
+      console.log(error)
+      setErr("검색중 오류 발생")
+      setItems([])
+    } finally {
+      setLoading(false);
+      inputRef.current?.focus()
+    }
+
+  }
+
   return (
     <div className='app'>
       <h1>위키백과 검색</h1>
-      <div className="error">에러코드</div>
+      <div className="error">에러 코드</div>
       <div className="search">
-        <input ref={inputRef} value={q} onChange={(e) => setQ(e.target.value)} type="text" placeholder='검색어를 입력하세요(예 : 서울, 한글, 리액트' />
-        <button>검색</button>
+        <input
+          ref={inputRef}
+          value={q}
+          onChange={(e) => setQ(e.target.value)}
+          onKeyUp={(e) => e.key === 'Enter' && handleSearch()}
+          type="text" placeholder='검색어를 입력하세요(예:서울, 한글, 리액트)' />
+        <button onClick={handleSearch} disabled={loading}>{loading ? "검색중..." : "검색"}</button>
       </div>
+      {err && <div className="error">에러코드</div>}
       <ul className='list'>
-        <li>
-          <div>
-            <strong>제목</strong>
-            <p>설명</p>
-            <a href="#">위키에서 보기</a>
-          </div>
-        </li>
-        <li>
-          <div>
-            <strong>제목</strong>
-            <p>설명</p>
-            <a href="#">위키에서 보기</a>
-          </div>
-        </li>
+        {items.map((p) => (
+
+          <li key={p.id}>
+            <div>
+              <strong>{p.title}</strong>
+              <p>{p.description || '요약없음'}</p>
+              <a href={`https://ko.wikipedia.org/wiki/${encodeURIComponent(p.title)}`}
+              target='_blank'>위키에서 보기</a>
+            </div>
+          </li>
+        ))}
+
       </ul>
     </div>
   )
--- src/api/wiki.js
+++ src/api/wiki.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const searchWikiTitles = async(q)=> {
-    const res = await axios.get("https://en.wikipedia.org/w/rest.php/v1/search/title", {
+export const searchWikiTitles = async(q)=> {
+    const res = await axios.get("https://ko.wikipedia.org/w/rest.php/v1/search/title", {
         params:{q, limit : 10}
     })
     return res.data.pages;
